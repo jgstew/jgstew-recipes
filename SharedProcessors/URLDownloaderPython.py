c1383fd20c4405a94f05b389bca84f01f13d52da
James Stewart <james.stewart@hcl.com>
2021-04-11 23:04:31 -0400

    making improvements

--- SharedProcessors/URLDownloaderPython.py
+++ SharedProcessors/URLDownloaderPython.py
@@ -5,8 +5,9 @@
 """See docstring for URLDownloaderPython class"""
 
 import os
-import tempfile
+# import tempfile
 import ssl
+import json
 
 from hashlib import sha1, sha256, md5
 
@@ -15,7 +16,7 @@ try:
 except ImportError:
     from urllib2 import urlopen  # Python 2
 
-import certifi
+import certifi  # pylint: disable=import-error
 
 from autopkglib import Processor, ProcessorError  # pylint: disable=import-error,wrong-import-position,unused-import
 from autopkglib.URLDownloader import URLDownloader  # pylint: disable=import-error
@@ -67,25 +68,49 @@ class URLDownloaderPython(URLDownloader):  # pylint: disable=invalid-name
     }
     __doc__ = description
 
-    def prefetch_filename(self):
+    def prefetch_filename(self):  # pylint: disable=no-self-use
         """Attempt to find filename in HTTP headers."""
         # need to implement this
         return False
 
-    def download_changed(self):
+    def download_changed(self):  # pylint: disable=no-self-use
         """Check if downloaded file changed on server."""
         # need to implement this - always True for now.
         return True
 
-    def ssl_context_ignore(self):
+    def store_download_info_json(self, download_dictionary):
+        """If file is downloaded, store info"""
+        pathname = self.env.get("pathname")
+        pathname_info_json = pathname + ".info.json"
+        # https://stackoverflow.com/questions/16267767/python-writing-json-to-file
+        with open(pathname_info_json, "w") as outfile:
+            json.dump(download_dictionary, outfile, indent=4)
+            # add newline at end of file:
+            outfile.write('\n')
+        return None
+
+    def get_download_info_json(self):
+        """get info from previous download"""
+        pathname = self.env.get("pathname")
+        pathname_info_json = pathname + ".info.json"
+
+        with open(pathname_info_json, "r") as infile:
+            info_json = json.load(infile)
+
+        return info_json
+
+    def ssl_context_ignore(self):  # pylint: disable=no-self-use
         """ssl context - ignore SSL validation"""
+        # this doesn't need to be a class method
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
+        self.output("WARNING: disabling SSL validation is insecure!!!")
         return ctx
 
-    def ssl_context_certifi(self):
+    def ssl_context_certifi(self):  # pylint: disable=no-self-use
         """ssl context using certifi CAs"""
+        # this doesn't need to be a class method
         # https://stackoverflow.com/questions/24374400/verifying-https-certificates-with-urllib-request
         return ssl.create_default_context(cafile=certifi.where())
 
@@ -108,10 +133,9 @@ class URLDownloaderPython(URLDownloader):  # pylint: disable=invalid-name
 
         # start download process:
         response = urlopen(url, context=self.ssl_context_certifi())
-        # NOTE: Get Header If Present for Download Estimate:
-        #               int(req.info().getheader('Content-Length').strip())
-        print("headers:")
-        print(response.info())
+
+        self.output("HTTP Headers: \n{headers}".format(
+            headers=response.info()), 2)
 
         while True:
             chunk = response.read(chunksize)
@@ -136,7 +160,13 @@ class URLDownloaderPython(URLDownloader):  # pylint: disable=invalid-name
         download_dictionary['file_sha256'] = hashes[1].hexdigest()
         download_dictionary['file_md5'] = hashes[2].hexdigest()
         download_dictionary['download_url'] = url
-        download_dictionary['http_headers'] = response.info()
+        #download_dictionary['http_headers'] = response.info()
+        download_dictionary['http_Content-Length'] = int(response.headers['content-length'])
+        download_dictionary['http_ETag'] = response.headers['ETag']
+        download_dictionary['http_Last-Modified'] = response.headers['Last-Modified']
+
+        # Save last-modified and etag headers to files xattr
+        self.store_headers(response.info())
 
         return download_dictionary
 
@@ -153,7 +183,15 @@ class URLDownloaderPython(URLDownloader):  # pylint: disable=invalid-name
         self.env["pathname"] = os.path.join(download_dir, filename)
         pathname_temporary = self.create_temp_file(download_dir)
 
+        previous_download_info = self.get_download_info_json()
+
+        self.output("previous_download_info: \n{previous_download_info}\n".format(
+            previous_download_info=previous_download_info), 2)
+
         download_dictionary = self.download_and_hash(pathname_temporary)
+
+        self.output("download_dictionary: \n{download_dictionary}\n".format(
+            download_dictionary=download_dictionary), 2)
 
         if self.download_changed():
             self.env["download_changed"] = True
@@ -165,8 +203,8 @@ class URLDownloaderPython(URLDownloader):  # pylint: disable=invalid-name
         # New resource was downloaded. Move the temporary download file to the pathname
         self.move_temp_file(pathname_temporary)
 
-        # Save last-modified and etag headers to files xattr
-        self.store_headers(download_dictionary['http_headers'])
+        # store download info for checking for existing download
+        self.store_download_info_json(download_dictionary)
 
         # Generate output messages and variables
         self.output(f"Downloaded {self.env['pathname']}")
@@ -174,6 +212,9 @@ class URLDownloaderPython(URLDownloader):  # pylint: disable=invalid-name
             "summary_text": "The following new items were downloaded:",
             "data": {"download_path": self.env["pathname"]},
         }
+
+        self.output("self.env: \n{self_env}\n".format(
+            self_env=self.env), 4)
 
 
 if __name__ == "__main__":

1601222e5f5c238d48c4daf614737466d5d31af8
James Stewart <james.stewart@hcl.com>
2021-04-17 14:35:15 -0400

    changing copyright comments to align with existing autopkglib Processors

--- SharedProcessors/URLDownloaderPython.py
+++ SharedProcessors/URLDownloaderPython.py
@@ -1,20 +1,30 @@
 #!/usr/local/autopkg/python
 #
-# James Stewart @JGStew - 2021
+# Copyright 2021 James Stewart @JGStew
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """See docstring for URLDownloaderPython class"""
 
 import json
 import os
 import ssl
-# import sys
-# import tempfile
 
 from hashlib import sha1, sha256, md5
 
 try:
     from urllib.request import urlopen  # Python 3
 except ImportError:
+    print("ERROR: Not tested with Python 2")
     from urllib2 import urlopen  # Python 2
 
 import certifi  # pylint: disable=import-error

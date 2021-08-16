1cd67704dab3b63d45bc64e660555b934999f6ef
jgstew <james@jgstew.com>
2021-08-16 17:56:41 -0400

    new processor, untested

---
+++ SharedProcessors/WinGetPropertyMSI.py
@@ -0,0 +1,69 @@
+#!/usr/local/autopkg/python
+"""
+See docstring for WinGetPropertyMSI class
+"""
+
+import msilib
+# import distutils.version
+
+from autopkglib import (  # pylint: disable=import-error,unused-import
+    Processor,
+    ProcessorError,
+    is_windows,
+)
+
+
+__all__ = ["WinGetPropertyMSI"]
+
+class WinGetPropertyMSI(Processor):  # pylint: disable=too-few-public-methods
+    """Gets the property the MSI file."""
+
+    description = __doc__
+    input_variables = {
+        "pathname": {
+            "required": True,
+            "description": ("MSI Filename to read from"),
+        },
+        "msi_property": {
+            "required": False,
+            "default": "ProductVersion",
+            "description": ("Name of the property to read from the Property table."
+                " Defaults to 'ProductVersion'"
+            ),
+        },
+    }
+    output_variables = {
+        "msi_property_value": {"description": "The value of the property if applicable."},
+    }
+
+    def get_property_msi(self, path, msi_property):
+        """read property from msi file"""
+        msi_db = msilib.OpenDatabase(path, msilib.MSIDBOPEN_READONLY)
+        view = msi_db.OpenView("SELECT Value FROM Property WHERE Property='" + msi_property + "'")
+        view.Execute(None)
+        result = view.Fetch()
+        self.output(dir(result), 3)
+        return result.GetString(1)
+
+    def main(self):
+        """execution starts here"""
+
+        if not msilib and not is_windows():
+            self.output("WARNING: This does not work on non-Windows", 0)
+            return ""
+
+        msi_filepath = self.env.get("pathname", None)
+        msi_property = self.env.get("msi_property", None)
+        msi_property_value = self.get_property_msi(msi_filepath, msi_property)
+
+        self.output(msi_property_value, 2)
+
+        self.env["msi_property_value"] = msi_property_value
+
+        if " " not in msi_property:
+            self.env[msi_property] = msi_property_value
+
+
+if __name__ == "__main__":
+    PROCESSOR = WinGetPropertyMSI()
+    PROCESSOR.execute_shell()

fa92b8297e6f59818da5cda2d9d5d8664b783f24
jgstew <james@jgstew.com>
2022-08-09 01:23:49 -0400

    tweak new processor

--- SharedProcessors/FileMsiGetProperty.py
+++ SharedProcessors/FileMsiGetProperty.py
@@ -3,6 +3,9 @@
 See docstring for FileMsiGetProperty class
 """
 
+import errno
+import os
+
 try:
     import msilib
 except ImportError:
@@ -57,7 +60,7 @@ class FileMsiGetProperty(Processor):  # pylint: disable=too-few-public-methods
         "file_msiinfo_ProductVersion": {"description": "ProductVersion"},
     }
 
-    def get_property_msi(self, path, msi_property):
+    def get_property_msi_msilib(self, path, msi_property):
         """read property from msi file"""
         # https://stackoverflow.com/a/9768876/861745
         msi_db = msilib.OpenDatabase(path, msilib.MSIDBOPEN_READONLY)
@@ -69,35 +72,104 @@ class FileMsiGetProperty(Processor):  # pylint: disable=too-few-public-methods
         # self.output(dir(result), 3)
         return result.GetString(1)
 
-    def get_property_msilib(self):
+    def get_properties_msilib(self):
         """for windows"""
         msi_path = self.env.get("msi_path", self.env.get("pathname", None))
         custom_msi_property = self.env.get("custom_msi_property", None)
         custom_msi_output = self.env.get("custom_msi_output", None)
 
-        self.env["file_msiinfo_ProductVersion"] = self.get_property_msi(
+        self.env["file_msiinfo_ProductVersion"] = self.get_property_msi_msilib(
             msi_path, "ProductVersion"
         )
 
-        self.env[custom_msi_output] = self.get_property_msi(
+        self.env[custom_msi_output] = self.get_property_msi_msilib(
             msi_path, custom_msi_property
         )
+
+    def verify_file_exists(self, file_path, raise_error=True):
+        """verify file exists, raise error if not"""
+        verbosity = 3
+        if raise_error:
+            verbosity = 0
+        if not file_path:
+            self.output("ERROR: no file_path provided!", verbosity)
+            if raise_error:
+                raise ProcessorError("No file_path provided!")
+        elif not os.path.isfile(file_path):
+            self.output(f"ERROR: file missing! `{file_path}`", verbosity)
+            if raise_error:
+                raise FileNotFoundError(
+                    errno.ENOENT, os.strerror(errno.ENOENT), file_path
+                )
+            return None
+        return file_path
+
+    def verify_file_executable(self, file_path, raise_error=False):
+        """verify file is executable"""
+        file_path_exists = self.verify_file_exists(file_path, raise_error)
+
+        if file_path_exists and os.access(file_path_exists, os.X_OK):
+            return True
+
+        return False
+
+    def get_msiinfo_path(self):
+        msiinfo_path_input = self.env.get("msiinfo_path", None)
+        msiinfo_path = None
+
+        path_array = ["/usr/bin/msiinfo", "/usr/local/bin/msiinfo"]
+        if msiinfo_path_input:
+            path_array.append(msiinfo_path_input)
+            # put provided path first
+            path_array.reverse()
+
+        for path in path_array:
+            if self.verify_file_executable(path):
+                msiinfo_path = path
+                break
+
+        return msiinfo_path
+
+    def get_properties_msiinfo(self):
+        """for non-windows
+
+        based upon:
+        - https://github.com/autopkg/hansen-m-recipes/blob/master/SharedProcessors/MSIInfoVersionProvider.py
+        """
+        # msi_path = self.env.get("msi_path", self.env.get("pathname", None))
+        # custom_msi_property = self.env.get("custom_msi_property", None)
+        # custom_msi_output = self.env.get("custom_msi_output", None)
+
+        msiinfo_path = self.get_msiinfo_path()
+
+        if not msiinfo_path:
+            raise ProcessorError(
+                """
+                msiinfo binary not found!
+                Install on MacOS with `brew install msitools`
+                Install on Ubuntu with `sudo apt-get install msitools -y`
+                """
+            )
+
+        self.output(msiinfo_path)
 
     def main(self):
         """execution starts here"""
         msi_path = self.env.get("msi_path", self.env.get("pathname", None))
+        self.verify_file_exists(msi_path)
         self.output(msi_path)
 
         if msilib:
             self.output("Info: `msilib` found! Must be running on Windows.", 3)
-            self.get_property_msilib()
+            self.get_properties_msilib()
         else:
             self.output(
                 "Info: `msilib` not found, assuming non-Windows. Attempting to use `msiinfo` instead.",
                 3,
             )
+            self.get_properties_msiinfo()
 
-        self.output("TODO: implement!", 0)
+        self.output("TODO: implement on non-windows!", 0)
 
 
 if __name__ == "__main__":

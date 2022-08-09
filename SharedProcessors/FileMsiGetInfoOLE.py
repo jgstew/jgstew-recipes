b16b9ee552eeeec75a5d7b4b0b97a5d5426de575
jgstew <james@jgstew.com>
2022-08-09 00:07:27 -0400

    working OLE processor

--- SharedProcessors/FileMsiGetInfoOLE.py
+++ SharedProcessors/FileMsiGetInfoOLE.py
@@ -31,27 +31,53 @@ class FileMsiGetInfoOLE(Processor):  # pylint: disable=too-few-public-methods
         },
         "custom_oleinfo_output": {
             "required": False,
-            "default": "olefile_author",
-            "description": "Variable to store the output to, defaults to `olefile_author`",
+            "default": "file_ole_author",
+            "description": "Variable to store the output to, defaults to `file_ole_author`",
         },
     }
     output_variables = {
-        "file_olefile_last_saved_time": {"description": "last_saved_time"},
+        "file_ole_max_time": {"description": "max creation last_saved time"},
+        "file_ole_create_time": {"description": "create_time"},
+        "file_ole_last_saved_time": {"description": "last_saved_time"},
+        "SourceReleaseDate": {"description": "max creation last_saved time yyyy-mm-dd"},
     }
+
+    def get_ole_max_time(self, ole_metadata):
+        """get max of creation and last_saved time"""
+        dates_to_compare = []
+
+        # get create_time
+        create_time = getattr(ole_metadata, "create_time")
+        self.env["file_ole_create_time"] = create_time
+        dates_to_compare.append(create_time)
+
+        # get last_saved_time
+        last_saved_time = getattr(ole_metadata, "last_saved_time")
+        self.env["file_ole_last_saved_time"] = last_saved_time
+        dates_to_compare.append(last_saved_time)
+
+        max_time = max(dates_to_compare)
+        self.env["file_ole_max_time"] = str(max_time)
+        self.env["SourceReleaseDate"] = max_time.strftime("%Y-%m-%d")
 
     def main(self):
         """execution starts here"""
         file_pathname = self.env.get("file_pathname", self.env.get("pathname", None))
+        custom_oleinfo_index = self.env.get("custom_oleinfo_index", None)
+        custom_oleinfo_output = self.env.get("custom_oleinfo_output", None)
         verbose = self.env.get("verbose", 3)
 
         oleinfo = olefile.OleFileIO(file_pathname)
-        meta = oleinfo.get_metadata()
+        ole_metadata = oleinfo.get_metadata()
+        self.get_ole_max_time(ole_metadata)
 
-        if verbose > 2:
+        self.env[custom_oleinfo_output] = str(
+            getattr(ole_metadata, custom_oleinfo_index).decode()
+        )
+
+        if verbose > 3:
             print("MSI OLE Summary Info Dump:")
-            meta.dump()
-
-        self.output("TODO: implement!", 0)
+            ole_metadata.dump()
 
         """
         Example OLEInfo:

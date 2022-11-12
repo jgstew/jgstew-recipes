45a67be6b7613e4da0e9688f84fc78f64eeea187
jgstew <james@jgstew.com>
2022-11-12 14:51:42 -0500

    tweaks, setup script

---
+++ setup_win.bat
@@ -0,0 +1,4 @@
+
+REM install chocolatey
+powershell -ExecutionPolicy Bypass -command "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))""
+

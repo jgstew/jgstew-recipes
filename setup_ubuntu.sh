b923dfb577597e0686e9f4a79ca38d360c33c032
jgstew <james@jgstew.com>
2023-07-04 21:11:52 -0400

    tweak setup script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,6 +1,9 @@
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y curl git wget python3 python3-pip build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
+python3 -m pip install --upgrade pip
+
 git clone https://github.com/autopkg/autopkg.git ../autopkg
+

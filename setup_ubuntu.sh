975371d54b2a8c6bb69378662b7570854ec51dc2
jgstew <james@jgstew.com>
2023-07-04 21:56:22 -0400

    add dependancies

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y curl git wget python3 python3-pip build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+sudo apt install -y p7zip-full msitools curl git wget python3 python3-pip build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
 # update python pip
 python3 -m pip install --upgrade pip
@@ -26,3 +26,6 @@ for line in $(cat .autopkg_repos.txt); do python3 ../autopkg/Code/autopkg repo-a
 
 # install jgstew-recipes requirements:
 python3 -m pip install --requirement requirements.txt
+
+# test:
+python3 autopkg/Code/autopkg run -vv Test-Recipes/AutopkgCore.test.recipe.yaml

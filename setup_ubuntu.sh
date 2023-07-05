958443436d004e4ae46ad0c954445fe66ab42ec4
jgstew <james@jgstew.com>
2023-07-04 21:08:18 -0400

    setup env ubuntu

---
+++ setup_ubuntu.sh
@@ -0,0 +1,6 @@
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install -y curl git wget python3 python3-pip build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+git clone https://github.com/autopkg/autopkg.git ../autopkg

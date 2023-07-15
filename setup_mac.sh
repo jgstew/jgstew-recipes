6779778a25a275686e31eab9f78f62eb303d7126
JGStew <james@jgstew.com>
2023-07-14 20:14:00 -0400

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -6,7 +6,7 @@
 exit 1
 
 # check xcode command line tools install:
-xcode-select --print-path
+# xcode-select --print-path
 
 # install homebrew:
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/96362c02f64bc1270645f6cd1698dda5a4790619/install.sh)"

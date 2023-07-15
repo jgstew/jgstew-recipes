b29db3d5db4eefbce9324a8f858f05246ce25402
JGStew <james@jgstew.com>
2023-07-14 20:12:16 -0400

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -8,7 +8,7 @@ xcode-select --print-path
 # install homebrew:
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/96362c02f64bc1270645f6cd1698dda5a4790619/install.sh)"
 
-NONINTERACTIVE=1 brew install python3.10 sevenzip msitools visual-studio-code
+NONINTERACTIVE=1 brew install python@3.10 sevenzip msitools visual-studio-code
 
 python3 -m pip install --upgrade pip 
 

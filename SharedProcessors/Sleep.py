0eb07f4d44859a5509b051511a1f4a50416b9a5c
jgstew <james@jgstew.com>
2023-11-29 09:36:16 -0500

    add sleep processor

---
+++ SharedProcessors/Sleep.py
@@ -0,0 +1,48 @@
+#!/usr/local/autopkg/python
+#
+# James Stewart @JGStew - 2021
+#
+# Related:
+# - https://github.com/jgstew/bigfix_prefetch/blob/master/prefetch_from_dictionary.py
+#
+"""See docstring for Sleep class"""
+
+import time
+
+from autopkglib import (  # pylint: disable=import-error,wrong-import-position,unused-import
+    Processor,
+    ProcessorError,
+)
+
+__all__ = ["Sleep"]
+
+
+class Sleep(Processor):  # pylint: disable=invalid-name
+    """Does nothing but delay autopkg for x seconds"""
+
+    description = __doc__
+    input_variables = {
+        "sleep_seconds": {
+            "required": False,
+            "default": 2,
+            "description": "seconds to sleep",
+        },
+    }
+    output_variables = {}
+    __doc__ = description
+
+    def main(self):
+        """Execution starts here"""
+
+        sleep_seconds = int(self.env.get("sleep_seconds", 2))
+
+        self.output(f"Pausing Execution for {sleep_seconds} seconds")
+
+        time.sleep(sleep_seconds)
+
+        self.output(f"Resuming Execution after {sleep_seconds} seconds")
+
+
+if __name__ == "__main__":
+    PROCESSOR = Sleep()
+    PROCESSOR.execute_shell()

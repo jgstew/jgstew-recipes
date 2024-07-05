970f91150bfd30195f750c99f2471b65e7849d59
JGStew <james@jgstew.com>
2024-07-05 00:42:58 -0400

    fix missing dep, fix test recipe

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -23,7 +23,7 @@ sudo python3.10 -m pip install --upgrade pip
 # update python basics
 sudo python3.10 -m pip install --upgrade setuptools wheel build
 
-sudo DEBIAN_FRONTEND=noninteractive apt install -y libcairo2-dev libmagic-dev jq p7zip-full msitools curl git wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+sudo DEBIAN_FRONTEND=noninteractive apt install -y speech-dispatcher libcairo2-dev libmagic-dev jq p7zip-full msitools curl git wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
 # This may solve a weird issue:
 # python3 -m pip install -U 'pyasn1<0.5.0'

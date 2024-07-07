c1d5e5d4cc99b31ad9f4177b967ca55322b7d03e
jgstew <james@jgstew.com>
2024-07-07 10:07:51 -0700

    create install script example

---
+++ Mozilla/Firefox-Win.installscript.ps1
@@ -0,0 +1,26 @@
+# URL of the file to download
+$url = "{{download_url}}"
+
+# Expected SHA256 hash of the file (replace with your actual hash)
+$expectedHash = "{{file_sha256}}"
+
+# Path where the downloaded file will be saved
+$outputFile = "C:\Windows\Temp\{{file_name}}"
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+
+# Calculate the actual SHA256 hash of the downloaded file
+$actualHash = Get-FileHash -Path $outputFile -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+
+# Compare the actual and expected hashes
+if ($actualHash -eq $expectedHash) {
+    Write-Output "Hashes match. Proceeding to execute the file."
+
+    # Execute the downloaded file
+    Start-Process -FilePath $outputFile -ArgumentList "/S"
+} else {
+    Write-Output "Hashes do not match. The downloaded file may be corrupted or tampered with."
+    # You can choose to delete the downloaded file in case of hash mismatch
+    # Remove-Item -Path $outputFile
+}
